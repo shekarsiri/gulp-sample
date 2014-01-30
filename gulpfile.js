2664ac4b34248e6246fb6985a572bcc0c4fce5b4
Shekar Siri <shekarsiri@27.132.205.49-ras.beamtele.net>
2014-01-30 14:33:48 +0530

    Added index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
-var gulp        = require('gulp'),
-    minify      = require('gulp-minify-css'),
-    less        = require('gulp-less'),
-    notify      = require('gulp-notify');
+var gulp            = require('gulp'),
+    minify          = require('gulp-minify-css'),
+    less            = require('gulp-less'),
+    notify          = require('gulp-notify')
+    prefix          = require('gulp-autoprefixer');
 
 gulp.task('styles', function(){
     gulp.src('./less/*.less')
     .pipe(less())
     .pipe(minify())
+    .pipe(prefix('last 15 version'))
     .pipe(gulp.dest('./css'))
     .pipe(notify({message:"All DONE SIR!"}));
 });
@@ -15,6 +17,6 @@ gulp.task('default', function(){
     gulp.run('styles');
 
     gulp.watch('./less/**/*.less', function(){
-        gulp.run('less');
+        gulp.run('styles');
     });
 })
